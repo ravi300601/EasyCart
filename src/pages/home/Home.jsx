724a12317a44bd718c12ab2797579fe576e0041e
ravi300601 <ravikb01.rb@gmail.com>
2024-08-11 21:13:18 +0530

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import Layout from '../../components/layout/Layout'
+import myContext from '../../context/data/myContext'
+import HeroSection from '../../components/heroSection/HeroSection'
+
+function Home() {
+
+    return (
+        <Layout>
+          <HeroSection/>
+        </Layout>
+    )
+}
+
+export default Home
---
+++ src/pages/nopage/NoPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout'
+
+function NoPage() {
+  return (
+    <Layout>NoPage</Layout>
+  )
+}
+
+export default NoPage
