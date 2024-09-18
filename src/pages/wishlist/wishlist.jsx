6063ea46bc1fb0e253b71d900c89df85e8e32922
ravi300601 <ravikb01.rb@gmail.com>
2024-09-18 16:00:54 +0530

    fixup wishlist

--- src/pages/wishlist/wishlist.jsx
+++ src/pages/wishlist/wishlist.jsx
@@ -6,93 +6,93 @@ import { ZmdiDelete } from "../../components/icons/icon";
 import myContext from "../../context/data/myContext";
 
 function Wishlist() {
-  const [loading, setLoading] = useState(true);
-  const [wishListItems, setWishListItems] = useState(() => {
-    return JSON.parse(localStorage.getItem("wishlist")) || [];
-  });
-  const context = useContext(myContext);
-  const { mode } = context;
+	const [loading, setLoading] = useState(true);
+	const [wishListItems, setWishListItems] = useState(() => {
+		return JSON.parse(localStorage.getItem("wishlist")) || [];
+	});
+	const context = useContext(myContext);
+	const { mode } = context;
 
-  const handleRemoveFromWishlist = (item) => {
-    const updatedWishListItems = wishListItems.filter(
-      (wItem) => wItem.id != item.id,
-    );
-    setWishListItems(updatedWishListItems);
-    localStorage.setItem("wishlist", JSON.stringify(updatedWishListItems));
-  };
-  useEffect(() => {
-    setLoading(false);
-  });
-  return (
-    <Layout>
-      {loading && <Loader />}
-      <div className="w-4/5 mx-auto my-5">
-        <table className="w-full">
-          <thead
-            className=""
-            style={{
-              border: mode === "dark" ? "white" : "",
-              color: mode === "dark" ? "white" : "",
-            }}
-          >
-            <tr className="">
-              <th className="py-3 text-left">Product</th>
-              <th className="py-3 md:table-cell"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {wishListItems?.map((item) => (
-              <>
-                <tr className="md:table-row">
-                  <td className="w-4/5 border hover:bg-slate-300/20">
-                    <div className="md:flex block items-center gap-x-10 w-full p-2">
-                      <Link to={"/productInfo/" + item.id}>
-                        <img
-                          src={item?.imageUrl}
-                          className="w-36 object-cover hover:opacity-80 md:mx-0 mx-auto"
-                          alt="404 not found"
-                        />
-                      </Link>
-                      <div>
-                        <Link to={"/productInfo/" + item.id}>
-                          <p
-                            className="text-gray-900 hover:text-slate-300"
-                            style={{ color: mode === "dark" ? "white" : "" }}
-                          >
-                            {item?.title}
-                          </p>
-                        </Link>
-                        <p
-                          className="text-gray-900"
-                          style={{ color: mode === "dark" ? "white" : "" }}
-                        >
-                          RS. {item.price}
-                        </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="p-5 border text-center hover:bg-slate-300/20">
-                    <button onClick={() => handleRemoveFromWishlist(item)}>
-                      <ZmdiDelete className="dark:text-red-400" />
-                    </button>
-                  </td>
-                </tr>
-              </>
-            ))}
+	const handleRemoveFromWishlist = (item) => {
+		const updatedWishListItems = wishListItems.filter(
+		(wItem) => wItem.id != item.id,
+		);
+		setWishListItems(updatedWishListItems);
+		localStorage.setItem("wishlist", JSON.stringify(updatedWishListItems));
+	};
+	useEffect(() => {
+		setLoading(false);
+	});
+	return (
+		<Layout>
+			{loading && <Loader />}
+			<div className="w-4/5 mx-auto my-5">
+				<table className="w-full">
+					<thead
+						className=""
+						style={{
+						border: mode === "dark" ? "white" : "",
+						color: mode === "dark" ? "white" : "",
+						}}
+					>
+						<tr className="">
+						<th className="py-3 text-left">Product</th>
+						<th className="py-3 md:table-cell"></th>
+						</tr>
+					</thead>
+					<tbody>
+						{wishListItems?.map((item) => (
+						<>
+							<tr className="md:table-row">
+							<td className="w-4/5 border hover:bg-slate-300/20">
+								<div className="md:flex block items-center gap-x-10 w-full p-2">
+								<Link to={"/productInfo/" + item.id}>
+									<img
+									src={item?.imageUrl}
+									className="w-36 object-cover hover:opacity-80 md:mx-0 mx-auto"
+									alt="404 not found"
+									/>
+								</Link>
+								<div>
+									<Link to={"/productInfo/" + item.id}>
+									<p
+										className="text-gray-900 hover:text-slate-300"
+										style={{ color: mode === "dark" ? "white" : "" }}
+									>
+										{item?.title}
+									</p>
+									</Link>
+									<p
+									className="text-gray-900"
+									style={{ color: mode === "dark" ? "white" : "" }}
+									>
+									RS. {item.price}
+									</p>
+								</div>
+								</div>
+							</td>
+							<td className="p-5 border text-center hover:bg-slate-300/20">
+								<button onClick={() => handleRemoveFromWishlist(item)}>
+								<ZmdiDelete className="dark:text-red-400" />
+								</button>
+							</td>
+							</tr>
+						</>
+						))}
 
-            {!wishListItems?.length && (
-              <tr>
-                <td>
-                  You haven't added any items to your wishlist. Discover
-                  something special today!
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </Layout>
-  );
+						{!wishListItems?.length && (
+						<tr>
+							<td style={{color: mode === "dark" ? "white" : ""}}>
+							You haven't added any items to your wishlist. Discover
+							something special today!
+							</td>
+						</tr>
+						)}
+					</tbody>
+				</table>
+			</div>
+		</Layout>
+	);
 }
 
 export default Wishlist;
